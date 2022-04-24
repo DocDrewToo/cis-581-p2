a66b94911d3383eebe246ca115f4e608893c3d88
Doc Drew <docdrew@umich.edu>
2022-04-24 15:38:50 -0400

    initial proj

---
+++ read-error.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+
+
+test_error_file = open('test_err_rate.dat', 'r+')
+test_error = []
+for line in test_error_file:
+    test_error.append(float(line.strip()))
+test_error_file.close()
+
+train_error_file = open('train_err.dat', 'r+')
+train_error = []
+for line in train_error_file:
+    train_error.append(line.strip())
+train_error_file.close()
+
+number_of_iterations = len(test_error)
+iterations = []
+for iteration in range(1, number_of_iterations+1):
+    iterations.append(iteration)
+
+plt.plot(test_error)
+# plt.xlabel('Iterations')
+# plt.ylabel('Test Error')
+plt.show()
